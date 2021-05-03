ff480276430b7b93570b0f22cd5859ad1c0d5a55
Gupta-Nikhil85 <73641466+Gupta-Nikhil85@users.noreply.github.com>
2021-05-03 19:54:57 +0530

    Added Drum Band Project
    This project uses event listener to match the key strokes.

---
+++ Drum Band/index.js
@@ -0,0 +1,58 @@
+var noOfButtons = document.querySelectorAll(".drum").length;
+for (var i = 0; i < noOfButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function(){
+    var buttonInnerHTML= this.innerHTML;
+    drumbeater(buttonInnerHTML);
+    buttonAnimation(buttonInnerHTML);
+  });
+}
+
+document.addEventListener("keydown",function(event){
+    drumbeater(event.key);
+    buttonAnimation(event.key);
+});
+
+function drumbeater(key){
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    default:
+
+  }
+}
+
+function buttonAnimation(currentkey){
+  var activeButton = document.querySelector("." + currentkey);
+  activeButton.classList.add("pressed");
+
+  setTimeout( function(){
+    activeButton.classList.remove("pressed");
+  } , 100);
+}
